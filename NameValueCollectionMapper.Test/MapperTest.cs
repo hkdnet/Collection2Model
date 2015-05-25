16acab743dd73c203eeddb5df641ed639ad77794
hkdnet <hkdnet@users.noreply.github.com>
2015-05-26 03:25:36 +0900

    InitialCommit

---
+++ NameValueCollectionMapper.Test/MapperTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NameValueCollectionMapper;
+using System.Collections.Specialized;
+
+namespace NameValueCollectionMapper.Test
+{
+    [TestClass]
+    public class MapperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new NameValueCollection();
+            c.Add("IntProp", "1");
+            c.Add("BoolProp", "true");
+            c.Add("StrPropUpper", "STRING");
+            c.Add("StrPropLower", "string");
+            c.Add("DoubleProp", "0.0");
+
+            var ret = NemeValueCollectionMapper.Mapper.Mapping<TestModel>(c);
+
+            Assert.AreEqual<int>(1, ret.IntProp);
+            Assert.AreEqual<bool>(true, ret.BoolProp);
+            Assert.AreEqual<String>("STRING", ret.StrPropUpper);
+            Assert.AreEqual<string>("string", ret.StrPropLower);
+            Assert.AreEqual<double>(0.0, ret.DoubleProp);
+
+        }
+    }
+    public class TestModel
+    {
+        public int IntProp { get; set; }
+        public bool BoolProp { get; set; }
+        public String StrPropUpper { get; set; }
+        public string StrPropLower { get; set; }
+        public double DoubleProp { get; set; }
+    }
+}
---
+++ NemeValueCollectionMapper/Mapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemeValueCollectionMapper
+{
+    public static class Mapper
+    {
+        public static T Mapping<T>(System.Collections.Specialized.NameValueCollection c)
+            where T : class, new()
+        {
+            var ret = new T();
+            var properties = typeof(T).GetProperties();
+            foreach (var p in properties)
+            {
+                var strVal = c[p.Name];
+                if (!p.CanWrite || strVal == null)
+                {
+                    continue;
+                }
+                if (p.PropertyType.FullName.Equals("System.String"))
+                {
+                    p.SetValue(ret, strVal, null);
+                }
+                var val = Convert.ChangeType(strVal, p.PropertyType);
+                p.SetValue(ret, val, null);
+            }
+            return ret;
+        }
+    }
+}
